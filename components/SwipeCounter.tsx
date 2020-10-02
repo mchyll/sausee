6f0d2a63b603bee1e1b7f507a95afa60adfab4ff
Truls Matias Torgersen <trulsmatias@gmail.com>
2020-10-02 14:57:25 +0200

    added prof of concept of tts

--- components/SwipeCounter.tsx
+++ components/SwipeCounter.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { Dimensions, ScrollView, TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import { CounterName } from "../shared/TypeDefinitions";
+import * as Speech from 'expo-speech';
 
 interface SwipeCounterProps {
   onChange: (name: CounterName, change: number) => void,
@@ -14,29 +15,49 @@ function SwipeCounter(props: SwipeCounterProps) {
     scrollViewRef.current?.scrollTo({ x: 0, y: Dimensions.get("window").height, animated: true });
   })
 
+  const change = (name:CounterName, change: number) => {
+    let toSay: string = "Ikke satt";
+    if(change === 1) toSay = "pluss"
+    else if(change === -1) toSay = "minus";
+    let theCount: number = props.count ?? 0;
+    let num: number = theCount + change;
+
+    Speech.speak(toSay);
+    Speech.speak("Det er nå " + num, );
+    Speech.speak(" antall " + props.name);  
+    props.onChange(props.name, change);
+  }
+
   return <ScrollView
     ref={scrollViewRef}
     pagingEnabled={true}
     onMomentumScrollEnd = {(e) => {
       const position = e.nativeEvent.contentOffset.y
       if (position === 0) {
+        change(props.name, -1);
+        console.log("Scrolled - minus");
+        /*
         props.onChange(props.name, -1);
         //scrollViewRef.current?.scrollTo({ x: 0, y: Dimensions.get("window").height, animated: true });
-
         console.log("- minus");
+        */
       }
       else if (position === Dimensions.get("window").height * 2) {
+        change(props.name, 1);
+        console.log("Scrolled + pluss");
+        /*
         props.onChange(props.name, 1);
         //scrollViewRef.current?.scrollTo({ x: 0, y: Dimensions.get("window").height, animated: true });
-
         console.log("+ pluss");
+        */
       }
     }}
   >
     <TouchableOpacity
       onPress={() => {
+        change(props.name, -1);
         console.log("Pressed red")
-        props.onChange(props.name, -1);
+        //props.onChange(props.name, -1);
       }}
     >
       <View style={styles.redBox}>
@@ -45,8 +66,12 @@ function SwipeCounter(props: SwipeCounterProps) {
     </TouchableOpacity>
     <TouchableOpacity
       onPress={() => {
+        change(props.name, 1);
         console.log("Pressed green")
-        props.onChange(props.name, 1);
+        /*props.onChange(props.name, 1);
+        Speech.speak("Hello world");
+        console.log("spoke");
+        */
       }}
     >
       <View style={styles.greenBox}>
