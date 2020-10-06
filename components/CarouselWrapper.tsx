462453f8564f467e46f3537459f530227d4d178d
Magnus Conrad Hyll <magnus@hyll.no>
2020-10-06 15:03:30 +0200

    Refactored CarouselWrapper

--- components/CarouselWrapper.tsx
+++ components/CarouselWrapper.tsx
@@ -2,30 +2,40 @@ import React, { useEffect, useRef } from 'react';
 import { Button, Dimensions, ScrollView, View } from "react-native";
 import SwipeCounter from "./SwipeCounter";
 import { CounterNameList, CounterName, Observation, SauseeState } from "../shared/TypeDefinitions"
-import { connect } from "react-redux"
+import { connect, ConnectedProps } from "react-redux"
 import { changeCounter } from '../shared/ActionCreators';
 
-interface InternalCarouselWrapperProps extends ExternalCarouselWrapperProps {
 
-  observation: Observation,
-  changeCounter: (countername: CounterName, change: number) => void,
-  onGoBack: () => void,
+const mapStateToProps = (state: SauseeState) => {
+  let trip = state.trips.find(trip => trip.id === state.currentTripId);
+  if (trip === undefined) throw new Error("Tried to mount component when currentTrip was undefined");
+
+  let observation = trip.observations.find(obs => obs.id == state.currentObservationId);
+  if (observation === undefined) throw new Error("Tried to mount component when currentObservation was undefined");
+
+  return {
+    observation,
+  }
 }
 
 interface ExternalCarouselWrapperProps extends CounterNameList {
   onGoBack: () => void,
 }
 
-const CarouselWrapper = (props: InternalCarouselWrapperProps) => {
+const connector = connect(mapStateToProps, { changeCounter });
+
+type CarouselWrapperProps = ConnectedProps<typeof connector> & ExternalCarouselWrapperProps
+
+const CarouselWrapper = (props: CarouselWrapperProps) => {
   const counterRef = useRef<ScrollView>(null);
   useEffect(() => {
-    counterRef.current?.scrollTo({ x: Dimensions.get("window").width * props.initCounterIndex, y:0, animated: false });
+    counterRef.current?.scrollTo({ x: Dimensions.get("window").width * props.initCounterIndex, y: 0, animated: false });
   }, []);
 
   return (
     <>
-      <View style={{borderWidth: 1, position:'absolute', top:80, left: 20, alignSelf:'flex-start', zIndex:10}} >
-        <Button title="Go back" onPress={() => props.onGoBack()}/>
+      <View style={{ borderWidth: 1, position: 'absolute', top: 80, left: 20, alignSelf: 'flex-start', zIndex: 10 }} >
+        <Button title="Go back" onPress={() => props.onGoBack()} />
       </View>
       <ScrollView ref={counterRef}
         horizontal={true}
@@ -37,23 +47,4 @@ const CarouselWrapper = (props: InternalCarouselWrapperProps) => {
   );
 }
 
-const mapStateToProps = (state: SauseeState, ownProps: ExternalCarouselWrapperProps) => {
-  let trip = state.trips.find(trip => trip.id === state.currentTripId);
-  if (trip === undefined) throw new Error("Tried to mount component when currentTrip was undefined");
-
-  let observation = trip.observations.find(obs => obs.id == state.currentObservationId);
-  if (observation === undefined) throw new Error("Tried to mount component when currentObservation was undefined");
-
-  return {
-    ...ownProps,
-    observation: observation,
-  }
-}
-
-const mapDispatchToProps = (dispatch: any) => { // todo: remove any
-  return {
-    changeCounter: (counterName: CounterName, change: number) => dispatch(changeCounter(counterName, change)),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CarouselWrapper);
+export default connector(CarouselWrapper);
