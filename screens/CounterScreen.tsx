3f3be51340bdf162c2d26c474f4074304c570ee9
Truls Matias Torgersen <trulsmatias@gmail.com>
2021-02-02 14:05:39 +0100

    seamless user experience

--- screens/CounterScreen.tsx
+++ screens/CounterScreen.tsx
@@ -51,11 +51,14 @@ const CounterScreen = (props: ConnectedProps<typeof connector> & StackScreenProp
   useEffect(() => {
     props.navigation.setOptions({ title: CounterDescriptions[currentCounter.current] });
     speak(CounterDescriptions[currentCounter.current])
-    let theInterval = setInterval(() => { speak(CounterDescriptions[currentCounter.current]) }, 9000);
+  }, [currentCounterIndex]);
+
+  useEffect(() => {
+    const theInterval = setInterval(() => { speak(CounterDescriptions[currentCounter.current]) }, 9000);
     return () => {
       clearInterval(theInterval);
     }
-  }, [currentCounterIndex]);
+  }, [currentCounterIndex, currentCount.current]);
 
   // Swaying animation on counter number as a visual cue for the possibility of swiping sideways
   // This animation will be stopped at once the user swipes horizontally (by horizontalPos.setValue)
