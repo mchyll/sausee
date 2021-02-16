5e999418407aabbcedd6202683f556c39f9928ae
Truls Matias Torgersen <trulsmatias@gmail.com>
2021-02-16 16:27:48 +0100

    added safe area to reseipt screen

--- screens/ReceiptScreen.tsx
+++ screens/ReceiptScreen.tsx
@@ -14,12 +14,13 @@ import { RoutePolyline } from '../components/RoutePolyline';
 import PrevObsPolylines from '../components/PrevObsPolylines';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 
 const mapStateToProps = (state: SauseeState) => {
   //currentTripId: state.currentTripId,
-  const trip = state.trips.find(trip => state.currentTripId === trip.id); 
+  const trip = state.trips.find(trip => state.currentTripId === trip.id);
   //if (!trip) trip = {}
   let observationTotal: ObservationCounters = {
     sheepCountTotal: 0,
@@ -65,103 +66,105 @@ const imageSize = 70; // was 100
 const margin = 10;
 const ReceiptScreen = (props: StartScreenProps) => {
   return (
-    <View style={{ justifyContent: "space-between", flexGrow: 1 }}>
-      <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: margin }}>
-        <View>
-          <Image
-            style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
-            source={require("../assets/multiple-sheep.png")}
-          />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.sheepCountTotal}</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{ justifyContent: "space-between", flexGrow: 1 }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: margin }}>
+          <View>
+            <Image
+              style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
+              source={require("../assets/multiple-sheep.png")}
+            />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.sheepCountTotal}</Text>
+          </View>
+          <View>
+            <Image
+              style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
+              source={require("../assets/sheep_1.png")}
+            />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.whiteGreySheepCount}</Text>
+          </View>
+          <View>
+            <Image
+              style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
+              source={require("../assets/brown-sheep.png")}
+            />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.brownSheepCount}</Text>
+          </View>
+          <View>
+            <Image
+              style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
+              source={require("../assets/black-sheep.png")}
+            />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.blackSheepCount}</Text>
+          </View>
         </View>
-        <View>
-          <Image
-            style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
-            source={require("../assets/sheep_1.png")}
-          />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.whiteGreySheepCount}</Text>
-        </View>
-        <View>
-          <Image
-            style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
-            source={require("../assets/brown-sheep.png")}
-          />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.brownSheepCount}</Text>
-        </View>
-        <View>
-          <Image
-            style={{ width: imageSize, height: imageSize, resizeMode: "contain", }}
-            source={require("../assets/black-sheep.png")}
-          />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.blackSheepCount}</Text>
-        </View>
-      </View>
 
-      <View style={{ flexDirection: "row", justifyContent: "space-around", marginBottom: margin }}>
-        <View>
-          <MaterialCommunityIcons name="tie" size={imageSize} color="#05d" />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.blueTieCount}</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-around", marginBottom: margin }}>
+          <View>
+            <MaterialCommunityIcons name="tie" size={imageSize} color="#05d" />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.blueTieCount}</Text>
+          </View>
+          <View>
+            <MaterialCommunityIcons name="tie" size={imageSize} color="#070" />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.greenTieCount}</Text>
+          </View>
+          <View>
+            <MaterialCommunityIcons name="tie" size={imageSize} color="#f4d528" />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.yellowTieCount}</Text>
+          </View>
+          <View>
+            <MaterialCommunityIcons name="tie" size={imageSize} color="#d22" />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.redTieCount}</Text>
+          </View>
+          <View style={{ justifyContent: "space-between" }}>
+            <AntDesign name="close" size={60} color="black" />
+            <Text style={{ alignSelf: "center" }}>{props.observationTotal.missingTieCount}</Text>
+          </View>
         </View>
-        <View>
-          <MaterialCommunityIcons name="tie" size={imageSize} color="#070" />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.greenTieCount}</Text>
-        </View>
-        <View>
-          <MaterialCommunityIcons name="tie" size={imageSize} color="#f4d528" />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.yellowTieCount}</Text>
-        </View>
-        <View>
-          <MaterialCommunityIcons name="tie" size={imageSize} color="#d22" />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.redTieCount}</Text>
-        </View>
-        <View style={{ justifyContent: "space-between" }}>
-          <AntDesign name="close" size={60} color="black" />
-          <Text style={{ alignSelf: "center" }}>{props.observationTotal.missingTieCount}</Text>
-        </View>
-      </View>
 
-      <View style={{ alignItems: "center", flexGrow: 1 }}>
-        <MapView
-          style={{ width: "80%", flexGrow: 1 }}
-          maxZoomLevel={20}
-          pitchEnabled={false}
-          provider="google"
-          showsUserLocation={true}
-          initialRegion={props.trip?.mapRegion}
-        >
-          <UrlTile urlTemplate="https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}" />
-
-
-          <RoutePolyline routePath={props.trip?.routePath} current={true} />
-          <PrevObsPolylines trip={props.trip} navToFormScreen={() => { }} current={true} />
-        </MapView>
-      </View>
+        <View style={{ alignItems: "center", flexGrow: 1 }}>
+          <MapView
+            style={{ width: "80%", flexGrow: 1 }}
+            maxZoomLevel={20}
+            pitchEnabled={false}
+            provider="google"
+            showsUserLocation={true}
+            initialRegion={props.trip?.mapRegion}
+          >
+            <UrlTile urlTemplate="https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}" />
 
 
+            <RoutePolyline routePath={props.trip?.routePath} current={true} />
+            <PrevObsPolylines trip={props.trip} navToFormScreen={() => { }} current={true} />
+          </MapView>
+        </View>
 
 
 
-      <View style={{ alignItems: "center", marginVertical: margin }}>
-        <Button
-          title={"Avslutt oppsynstur"}
-          onPress={() => {
-            Alert.alert("Avslutt oppsynstur", "Er du sikker?", [
-              { text: "Avbryt", style: "cancel" },
-              {
-                text: "OK", onPress: () => {
-                  props.finishTrip();
-                  stopRouteTracking();
-                  props.navigation.reset({
-                    index: 0,
-                    routes: [{ name: "StartScreen" }]
-                  });
+
+
+        <View style={{ alignItems: "center", marginVertical: margin }}>
+          <Button
+            title={"Avslutt oppsynstur"}
+            onPress={() => {
+              Alert.alert("Avslutt oppsynstur", "Er du sikker?", [
+                { text: "Avbryt", style: "cancel" },
+                {
+                  text: "OK", onPress: () => {
+                    props.finishTrip();
+                    stopRouteTracking();
+                    props.navigation.reset({
+                      index: 0,
+                      routes: [{ name: "StartScreen" }]
+                    });
+                  }
                 }
-              }
-            ]);
-          }}
-        />
+              ]);
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
