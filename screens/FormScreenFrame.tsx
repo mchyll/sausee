1585c051ffb89c91d946bbbd62819ec02f3ea5d1
Magnus Conrad Hyll <magnus@hyll.no>
2021-03-17 14:50:29 +0100

    Replace header buttons with "Ferdig" on previous observations

--- screens/FormScreenFrame.tsx
+++ screens/FormScreenFrame.tsx
@@ -1,16 +1,17 @@
-import { StackNavigationProp } from "@react-navigation/stack";
+import { HeaderBackButton, StackNavigationProp } from "@react-navigation/stack";
 import React, { useEffect } from "react";
 import { Alert, Button, Platform, ScrollView, View } from "react-native";
 import { connect, ConnectedProps } from "react-redux";
 import { RootStackParamList, SauseeState } from "../shared/TypeDefinitions";
-import { deleteObservation, finishObservation } from "../shared/ActionCreators";
+import { deleteObservation, finishObservation, cancelObservation } from "../shared/ActionCreators";
 import { Button as MaterialButton } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const connector = connect((state: SauseeState) => ({
   isNewObservation: state.currentObservation?.isNewObservation,
   editable: state.currentObservation?.editable ?? false
-}), { deleteObservation, finishObservation });
+}), { deleteObservation, finishObservation, cancelObservation });
 
 function FormScreenFrame<TRoute extends keyof RootStackParamList>(props: React.PropsWithChildren<ConnectedProps<typeof connector> & {
   navigation: StackNavigationProp<RootStackParamList, TRoute>,
@@ -21,7 +22,30 @@ function FormScreenFrame<TRoute extends keyof RootStackParamList>(props: React.P
     props.navigation.setOptions({
       headerTitle: props.isNewObservation ? "Ny observasjon" : "Tidligere observasjon"
     });
-  }, [props.navigation, props.isNewObservation]);
+
+    // Replace header buttons with "Ferdig" on previous observations
+    if (!props.editable) {
+      const onFinish = () => {
+        // This must be called before navigating away, since we try to finish the observation when the screen is closed
+        props.cancelObservation();
+        props.navigation.goBack();
+      };
+
+      props.navigation.setOptions({
+        headerLeft: Platform.select({
+          ios: () => <Button
+            title="Ferdig"
+            onPress={onFinish}
+          />,
+          default: () => <HeaderBackButton
+            backImage={() => <MaterialIcons name="close" size={26} color="black" />}
+            onPress={onFinish}
+          />
+        }),
+        headerRight: undefined
+      });
+    }
+  }, [props.navigation, props.isNewObservation, props.editable]);
 
   useEffect(() => {
     return props.navigation.addListener("beforeRemove", () => {
